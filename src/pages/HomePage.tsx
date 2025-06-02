6669738940d2eb597cfaf25855409cc85242bd30
VMVT\lukas.orliukas <lukas.orliukas@vmvt.lt>
2025-06-02 09:40:46 +0300

    added date function and fixed error message

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -19,12 +19,15 @@ export const HomePage = () => {
   const { t, i18n } = useTranslation();
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const [showFilters, setShowFilters] = useState(false)
+  const [showFilters, setShowFilters] = useState(false);
 
   const q = searchParams.get("q") || ""; // q == query
   const p = searchParams.get("p") || 1; // p == page
+  const today = new Date();
 
-  const [isUPD, setIsUPD] = useState(localStorage.getItem("isUPD") === 'true' || false);
+  const [isUPD, setIsUPD] = useState(
+    localStorage.getItem("isUPD") === "true" || false
+  );
 
   useEffect(() => {
     if (isNaN(Number(p)) || Number(p) < 0) {
@@ -35,44 +38,47 @@ export const HomePage = () => {
     }
   }, [q, p, setSearchParams]);
 
+  const { data: medicine, isLoading } = useAllMedicines(
+    q,
+    Number(p),
+    isUPD,
+    i18n.language
+  );
+  const { data: filters } = useFilters(i18n.language);
 
-  const { data: medicine, isLoading } = useAllMedicines(q, Number(p), isUPD, i18n.language);
-  const { data: filters} = useFilters(i18n.language);
+  console.log(filters);
 
-  console.log(filters)
-
-
-  
-  console.log(medicine?.data)
+  console.log(medicine?.data);
 
   const medicineSchema = Yup.object().shape({
-    medicine: Yup.string().test(
-      function (value) {
-        if (!value || value.length > 2) {
-          return true;
-        }
-          // arba galima padaryti su REGEX
-        return this.createError({
-          message: t('validation.medicineLength', { value }),
-        });
+    medicine: Yup.string().test(function (value) {
+      if (!value || value.length > 2) {
+        return true;
       }
-    )
+      // arba galima padaryti su REGEX
+      return this.createError({
+        message: "validation.medicineLength",
+      });
+    }),
   });
 
   const formValues = { medicine: q };
 
   const handleSubmit = (
-    values: typeof formValues, 
-    { resetForm }: { resetForm: (nextState?: Partial<FormikState<typeof formValues>>) => void }
+    values: typeof formValues,
+    {
+      resetForm,
+    }: {
+      resetForm: (nextState?: Partial<FormikState<typeof formValues>>) => void;
+    }
   ) => {
-  setSearchParams({
-    q: values.medicine,
-    p: "1",
-  });
+    setSearchParams({
+      q: values.medicine,
+      p: "1",
+    });
 
-
-  resetForm({ values: values, isSubmitting: false, isValidating: false });
-};
+    resetForm({ values: values, isSubmitting: false, isValidating: false });
+  };
 
   const handlePageChange = (newPage: number) => {
     setSearchParams({
@@ -81,22 +87,12 @@ export const HomePage = () => {
     });
   };
 
-
-  console.log("current p: ", p)
-
-
-  // const handleDateDifference = (registrationDate:string) => {
-  //    const dateObj = new Date();
-  //   const month   = dateObj.getUTCMonth() + 1;
-  //   const day     = dateObj.getUTCDate();
-  //   const year    = dateObj.getUTCFullYear();
-
-  //   const currentDate = `${year}-${month}-${day}`;
-  //   const currentDateArray = currentDate.split('-')
-  //   const pastDateArray = registrationDate.split('-')
-
-  //  
-  // }
+  const handleDateDifference = (registrationDate: string) => {
+    const pastDate = new Date(registrationDate);
+    const monthLater = new Date(pastDate);
+    monthLater.setMonth(monthLater.getMonth() + 1);
+    return today < monthLater;
+  };
 
   return (
     <main>
@@ -132,7 +128,10 @@ export const HomePage = () => {
           type={"checkbox"}
           checked={isUPD}
           onChange={(e) => {
-            return setIsUPD(e.target.checked), localStorage.setItem("isUPD", e.target.checked.toString());
+            return (
+              setIsUPD(e.target.checked),
+              localStorage.setItem("isUPD", e.target.checked.toString())
+            );
           }}
         />
       </form>
@@ -140,7 +139,7 @@ export const HomePage = () => {
         <LeftColumn>
           {medicine !== undefined && medicine?.items !== 0 && (
             <>
-              <StyledFilters  data={filters}/>
+              <StyledFilters data={filters} />
               <ShowFilters onClick={() => setShowFilters((prev) => !prev)}>
                 <Icon name={"filters"} />
                 Rodyti Filtrus
@@ -152,16 +151,16 @@ export const HomePage = () => {
                   setShowFilters(false);
                 }}
               >
-                <Filters data={filters}/>
+                <Filters data={filters} />
               </PopUp>
             </>
           )}
         </LeftColumn>
         <RightColumn>
-          {isLoading ?  <Loader /> : ""}
+          {isLoading ? <Loader /> : ""}
           {medicine?.items !== 0 ? (
             medicine?.data?.map((item) => {
-              // handleDateDifference(item.date)
+              //handleDateDifference(item.date)
               return (
                 <Medicine
                   key={item.id}
@@ -169,7 +168,7 @@ export const HomePage = () => {
                   subtitle={item.ingredients}
                   code={item.code}
                   title={item.name}
-                  isNew={false}
+                  isNew={handleDateDifference(item.date)}
                   tags={item.species}
                   onClick={() =>
                     navigate(slugs.medicineDetail(item.id), {
@@ -200,26 +199,26 @@ const StyledFilters = styled(Filters)`
   @media ${device.mobileL} {
     display: none;
   }
-`
+`;
 const ShowFilters = styled.button`
   gap: 12px;
   display: none;
   padding: 14.5px 20px;
   border-radius: 26px;
-  border: 1px solid ${({theme}) => theme.colors.grey_light};
-  color: ${({theme}) => theme.colors.primary};
+  border: 1px solid ${({ theme }) => theme.colors.grey_light};
+  color: ${({ theme }) => theme.colors.primary};
   font-weight: 500;
   font-family: "Inter";
   font-size: 1rem;
   margin-bottom: 16px;
-  
+
   &:hover {
-    background-color: ${({theme}) => theme.colors.secondary};
+    background-color: ${({ theme }) => theme.colors.secondary};
   }
   @media ${device.mobileL} {
     display: flex;
   }
-`
+`;
 
 const NotFound = styled.p`
   font-weight: 500;
@@ -238,12 +237,10 @@ const LeftColumn = styled.div`
   width: 30%;
   @media ${device.mobileL} {
     width: 100%;
-    
   }
-  & > div > div:first-of-type{
-    padding-top: 0px 
+  & > div > div:first-of-type {
+    padding-top: 0px;
   }
-
 `;
 const RightColumn = styled.div`
   width: 70%;
